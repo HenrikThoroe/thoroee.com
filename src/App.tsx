576942da453ff56e301675e71e60777fdf7a1a67
Henrik Thoroe <henrik@thoroee.com>
2020-06-20 20:08:31 +0200

    Favicon & Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Page from './views/Page'
+import { Helmet } from "react-helmet"
 
 function App() {
-    return <Page />
+    return (
+        <>
+            <Helmet>
+                <title>Thorøe</title>
+            </Helmet>
+            <Page />
+        </>
+    )
 }
 
 export default App;
