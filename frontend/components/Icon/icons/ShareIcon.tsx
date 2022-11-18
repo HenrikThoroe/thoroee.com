16dafddc380ed42af53743d463cba43c0f28fae9
Henrik Thoroe <henrik@thoroee.com>
2022-11-18 22:27:44 +0100

    fix(comp): use react style params

--- frontend/components/Icon/icons/ShareIcon.tsx
+++ frontend/components/Icon/icons/ShareIcon.tsx
@@ -11,25 +11,25 @@ export default function ShareIcon(props: React.SVGProps<SVGSVGElement>) {
         className="icon-path"
         d="M3.16666 9.5V15.8333C3.16666 16.2533 3.33347 16.656 3.6304 16.9529C3.92734 17.2499 4.33006 17.4167 4.74999 17.4167H14.25C14.6699 17.4167 15.0726 17.2499 15.3696 16.9529C15.6665 16.656 15.8333 16.2533 15.8333 15.8333V9.5"
         stroke="#000"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         className="icon-path"
         d="M12.6667 4.75001L9.50001 1.58334L6.33334 4.75001"
         stroke="#000"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         className="icon-path"
         d="M9.5 1.58334V11.875"
         stroke="#000"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
